f72317ecbe086360dc203a739247b6c0ca67e859
Leandro Sena Zuza <leandrosenazuza@gmail.com>
2022-10-12 15:47:19 -0300

    primeiro

---
+++ src/main/java/br/com/leandrosenazuza/DessafioBancoDigitalDio/Application/DessafioBancoDigitalDioApplication.java
@@ -0,0 +1,13 @@
+package br.com.leandrosenazuza.DessafioBancoDigitalDio.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DessafioBancoDigitalDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DessafioBancoDigitalDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/leandrosenazuza/DessafioBancoDigitalDio/DessafioBancoDigitalDioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leandrosenazuza.DessafioBancoDigitalDio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DessafioBancoDigitalDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
